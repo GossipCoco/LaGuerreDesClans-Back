6a3b75b51306ea6ffcccdf98d476e3d542d00026
Coralie Honorin <gossipcoco@hotmail.fr>
2024-09-29 15:49:54 +0200

    controllers up grade and quest

--- app/controllers/grade.js
+++ app/controllers/grade.js
@@ -1,15 +1,8 @@
 const Grade = {};
-
-const queries = require('../Queries/GradeQueries')
+const { handleResponse } = require("./function");  // Importer la fonction
+const query = require('../Queries/GradeQueries')
 
 Grade.GetAllGrades = (req, res) => {
-    queries.GetAllGrades(req, res)
-    .then(w => {
-        res.send({ob: w, res: true}).status(200)
-    })
-    .catch(err => {
-        console.log(err)
-        res.send(err).status(500)
-    })
+    handleResponse(res, query.GetAllGrades(req, res))
 }
 module.exports = Grade;
