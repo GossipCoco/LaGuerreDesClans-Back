da972da800057147f00f577abc77c25601c444f8
Coralie Honorin <gossipcoco@hotmail.fr>
2024-10-17 09:20:34 +0200

    up image characters

--- app/routes/Chapter.js
+++ app/routes/Chapter.js
@@ -1,16 +1,16 @@
 const express = require('express');
 
-const Fiction = require('../controllers/chapter');
+const Chapter = require('../controllers/chapter');
 
  // Assume this is your multer config
 const createUploadMiddleware = require('../middleware/UploadGeneric');
 const router = express.Router();
 
 router
-.get('/GetFiveLastChapByUser/:id', Fiction.GetFiveLastChapByUser)
-.post('/Chapitre/:id', Fiction.GetAChapterByName)
-.post('/GetLastChapterOfAFiction/:id', Fiction.GetLastChapterOfAFiction)
+.get('/GetFiveLastChapByUser/:id', Chapter.GetFiveLastChapByUser)
+.post('/Chapitre/:id', Chapter.GetAChapterByName)
+.post('/GetLastChapterOfAFiction/:id', Chapter.GetLastChapterOfAFiction)
 .post('/CreateAChapitre/:id', createUploadMiddleware('Fictions/').single('image'), Fiction.CreateANewChapter)
-.post('/EditChapter/:id', Fiction.EditChapter)
+.post('/EditChapter/:id', Chapter.EditChapter)
 
 module.exports = router;
